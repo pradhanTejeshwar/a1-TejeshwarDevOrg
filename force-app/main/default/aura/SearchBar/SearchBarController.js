0568b09c1449155dcd191abbc4625e0a53dbd3db
Tejeshwar Pradhan <t1999pradhan@gmail.com>
2024-09-24 17:18:10 +0530

    Revert "New Changes from 29042024"

--- force-app/main/default/aura/SearchBar/SearchBarController.js
+++
@@ -1,9 +0,0 @@
-({
-	searchKeyChange : function(component, event, helper) 
-    {
-        var myEvent = $A.get("e.c:SearchKeyChange");
-        
-        myEvent.setParams({"searchKey":event.target.value});
-        myEvent.fire();
-	}
-})
